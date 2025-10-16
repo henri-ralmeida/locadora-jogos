f463e67b482d191d0255069fdd1024b7349e4b1c
Henrique <henri.ralmeida@protonmail.com>
2025-10-16 18:40:55 -0300

    start na aplicacao do java com spring boot initializr

---
+++ src/main/java/br/com/retro/locadorajogos/LocadoraJogosApplication.java
@@ -0,0 +1,13 @@
+package br.com.retro.locadarorajogos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocadarorajogosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocadarorajogosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/retro/locadorajogos/LocadoraJogosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.retro.locadarorajogos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocadarorajogosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
